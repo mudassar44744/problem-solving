5e1bcc3f49941d9d07577b05dadc6ae9e527f353
Mudassar Mustafai <mmustaffi@3ssolutions.net>
2023-05-04 12:49:38 +0500

    factorial examples

---
+++ factorial.js
@@ -0,0 +1,31 @@
+//Write a program to find the factorial of the number.
+//Iterative method
+
+let number = 4;
+
+let value = recursiveFactorial(number);
+
+console.log(value);
+
+
+function factorial(number) {
+
+if(number <= 0) return undefined;
+
+let fact = 1;
+
+for (let index = 1; index <= number; index++) {
+    fact *= index; // fact = fact * index;
+    
+}
+return fact;
+
+}
+
+function recursiveFactorial(number) {
+    
+    if(number === 0) return 1;
+
+    return   (number *  recursiveFactorial(number - 1));
+
+}
