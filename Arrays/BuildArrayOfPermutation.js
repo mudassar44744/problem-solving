42d1ae13332d4df57715c70e3ffa76782e549907
unknown <=>
2023-09-19 12:07:38 +0500

    array permutation

---
+++ Arrays/BuildArrayOfPermutation.js
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/build-array-from-permutation
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    var ans = [];
+  for (var i = 0; i < nums.length; i++) {
+      ans[i] = nums[nums[i]];
+  }
+  return ans;
+};
